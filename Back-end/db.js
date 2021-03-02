eac8990f1ff7bc539879524a3d49bdc3fc6594d7
HugoPoletto34 <hugopoletto34@gmail.com>
2021-03-02 20:46:56 -0300

    Deploy at Heroku

--- Back-end/db.js
+++ Back-end/db.js
@@ -1,13 +1,17 @@
 const Pool=require('pg').Pool
 require('dotenv/config')
+const dev = {
+    user: process.env.DB_USERNAME, 
+    host: process.env.DB_HOST, 
+    password: process.env.DB_PASSWORD, 
+    database: process.env.DB_DATABASE, 
+    port: 5432    
+}
 
-const pool = new Pool({ 
-    host: process.env.DB_HOST, 
-    user: process.env.DB_USERNAME, 
-    password: process.env.DB_PASSWORD, 
-    database: process.env.DB_DATABASE, 
-    port: 5432, 
+const prod = {
+    url: process.env.DATABASE_URL
+}
 
-}); 
+const pool = new Pool(dev); 
 
 module.exports = pool
