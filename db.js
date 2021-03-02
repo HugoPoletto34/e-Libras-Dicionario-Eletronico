8676a29f65858e412183ef941501f7a0579eee6d
HugoPoletto34 <hugopoletto34@gmail.com>
2021-03-02 19:08:01 -0300

    test deploy postgres

--- db.js
+++ db.js
@@ -1,13 +1,19 @@
-const Pool=require('pg').Pool
+const Pool = require('pg').Pool
 require('dotenv/config')
 
-const pool = new Pool({ 
-    host: process.env.DB_HOST, 
+const devDataBase = {
+    host: process.env.DB_HOST, 
     user: process.env.DB_USERNAME, 
     password: process.env.DB_PASSWORD, 
     database: process.env.DB_DATABASE, 
     port: 5432, 
+}
 
-}); 
+const buildDataBase = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+}
+
+const pool = new Pool(buildDataBase); 
 
 module.exports = pool
