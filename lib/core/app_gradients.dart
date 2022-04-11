51bd4e93ec29cae2697a8edeae9f7b547e70271f
HugoPoletto34 <hugopoletto34@gmail.com>
2022-04-11 16:00:26 -0300

    Add new images

--- lib/core/app_gradients.dart
+++ lib/core/app_gradients.dart
@@ -5,8 +5,8 @@ import 'package:flutter/material.dart';
 
 class AppGradients {
   static final linear = LinearGradient(colors: [
-    Color.fromARGB(255, 87, 181, 228),
-    Color.fromARGB(176, 0, 90, 192),
+    Color.fromARGB(255, 131, 214, 255),
+    Color.fromARGB(246, 29, 114, 194),
   ], stops: [
     0.0,
     0.695
--- lib/splash/splash_page.dart
+++ lib/splash/splash_page.dart
@@ -5,8 +5,9 @@ import 'package:flutter/material.dart';
 class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Future.delayed(Duration(seconds: 2)).then((value) => Navigator.pushReplacement(
-        context, MaterialPageRoute(builder: (context) => HomePage())));
+    Future.delayed(Duration(seconds: 2)).then((value) =>
+        Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => HomePage())));
 
     return Scaffold(
       body: Container(
