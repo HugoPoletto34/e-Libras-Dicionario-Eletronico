2b6b61710c35505a8455745ffcbc1caf92556812
HugoPoletto34 <hugopoletto34@gmail.com>
2023-01-16 09:50:11 -0300

    commit

--- pages/dicionario/index.tsx
+++ pages/dicionario/index.tsx
@@ -1,14 +1,14 @@
 import { Palavra } from '../../data/entities/Palavra';
 import ListaDicionario from '../../presentation/components/ListaDicionario';
-
+import style from "../../styles/Home.module.css";
 
 
 export default function Dicionario({ filteredList } : {filteredList: Palavra[]}) {
   
   return (
-    <>
-    <ListaDicionario filteredList={filteredList} doOnClick={() => null}/>
+    <div className={style.dicionarioPrincipal}>
+      <ListaDicionario filteredList={filteredList} doOnClick={() => null}/>
     
-    </>
+    </div>
   );
 }
--- presentation/components/ListaDicionario.tsx
+++ presentation/components/ListaDicionario.tsx
@@ -3,33 +3,65 @@ import router from "next/router";
 import { Palavra } from "../../data/entities/Palavra";
 import style from "../../styles/Home.module.css";
 
+interface Ssa {
+  letra: string;
+  lista: Palavra[];
+}
+
+const listaAlfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+function mapearListaDePalavrasPorOrdemAlfabetica(lista: Palavra[]) {
+  let listaDePalavrasPorOrdemAlfabetica: Ssa[] = [];
+  listaAlfabeto.forEach((letra) => {
+      listaDePalavrasPorOrdemAlfabetica.push({ letra, lista: lista.filter((palavra) => palavra.nomePalavra.charAt(0) === letra) });
+  });
+  return listaDePalavrasPorOrdemAlfabetica;
+}
+
 export default function ListaDicionario({ filteredList, doOnClick }: {filteredList: Palavra[], doOnClick: () => void}) {
+  const listaDePalavrasPorOrdemAlfabetica = mapearListaDePalavrasPorOrdemAlfabetica(filteredList);
+  // console.log(listaDePalavrasPorOrdemAlfabetica)
   return (
     <>
-      {filteredList != null  ? <List>
-      {filteredList.map(palavra => {
+      {listaDePalavrasPorOrdemAlfabetica != null  ? <List>
+      {listaDePalavrasPorOrdemAlfabetica.map(palavra => {
         return (
-          <>
-          <ListItem alignItems="center" key={palavra.nomePalavra} className={style.asd}>
-  
-            <Card sx={{ width: "100%" }}>
-              <CardActionArea onClick={() => {
-                router.push(`/dicionario/${palavra.nomePalavra}`);
-                doOnClick();
-              }}>
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    {palavra.nomePalavra}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {palavra.descPalavra}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </ListItem>
-          <Divider variant="inset" component="li" />
-          </>
+          <div key={palavra.letra}>
+            {palavra.lista.length > 0 ? 
+            <>
+              <Typography variant="h6" className={style.titleLetra}>
+                {palavra.letra}
+              </Typography>
+              {palavra.lista.map(palavra => {
+                return (
+                  <>
+                    <ListItem alignItems="center" key={palavra.nomePalavra} className={style.containerCartao}>
+            
+                      <Card className={style.cartao}>
+                        <CardActionArea onClick={() => {
+                          router.push(`/dicionario/${palavra.nomePalavra}`);
+                          doOnClick();
+                        }}>
+                          <CardContent>
+                            <div className={style.cardTitle} >
+                              <img width={80} src={"https://" + palavra.imageFileName} alt={palavra.nomePalavra} />
+                              <Typography gutterBottom variant="h5" component="h5">
+                                {palavra.nomePalavra}
+                              </Typography>
+                            </div>
+                            <Typography variant="body2" color="text.secondary" className={style.descPalavra}>
+                              {palavra.descPalavra}
+                            </Typography>
+                            <img className={style.imageLibras} width={40} src={"/libras.png"} alt={palavra.nomePalavra} />
+                          </CardContent>
+                        </CardActionArea>
+                      </Card>
+                    </ListItem>
+                    <Divider variant="inset" component="li" />
+                  </>
+                )
+              })}
+            </> : <></>}
+          </div>
         )
       })}
       
@@ -37,3 +69,5 @@ export default function ListaDicionario({ filteredList, doOnClick }: {filteredLi
     </>
   );
 }
+
+{/*  */}
